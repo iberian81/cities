8465f1e21beaf2f448f7f733701814a285ae5f9e
iberian81 <a@outlook.com>
2019-06-01 19:01:31 +0200

    first lines in frontend

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, RouterLink, NavigationExtras } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public title: String = "Cities";
+
+  
+  public cityList=[];
+
+constructor(public httpClient : HttpClient, public router: Router){
+  
+}
+
+
+ionViewDidEnter(){
+  this.httpClient.get("http://localhost:3000/villes/").subscribe(
+    (data: any) => {
+      this.cityList = data;
+      console.log(this.cityList);
+    },
+    err => console.log(err)
+  );   
+}
+
+
+deleteCity(id){
+  this.httpClient.delete('http://localhost:3000/ville/'+id).subscribe(
+    ()=>{
+      this.ionViewDidEnter();
+    },
+    err => console.log(err)
+  );
+}
+
+showDetail(id){
+  this.router.navigateByUrl('/detail/'+id);
+}
+
+
+updateCity(id){
+  this.httpClient.put('http://localhost:3000/ville', id).subscribe(
+    ()=>console.log("update ok")
+  );
+}
+
+
 
 }
